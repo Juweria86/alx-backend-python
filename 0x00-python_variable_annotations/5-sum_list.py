24a4baa047be8943a47a145dbc16b2949242ed14
Juweria86 <jujumelmi143@gmail.com>
2024-07-04 13:28:52 +0300

    fixed pycodestyle error

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """This module returns the sum of list"""
+from typing import List
 
 
-def sum_list(input_list: list[float]):
+def sum_list(input_list: List[float]):
     """returns sum of list"""
 
     return sum(input_list)
