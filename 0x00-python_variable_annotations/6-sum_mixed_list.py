a44dbd50a59102d26c90b10674d1c1634b03af62
Juweria86 <jujumelmi143@gmail.com>
2024-07-04 13:43:03 +0300

    mixed List

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """This module return sum of mixed list"""
-from typing import List
+from typing import List, union
 
 
-def sum_mixed_list(mxd_lst: List[float]) -> float:
+def sum_mixed_list(mxd_lst: List[union[float]]) -> float:
     """returns sum of mixed type of list"""
 
     return sum(mxd_lst)
